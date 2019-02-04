9b8a7629d22f631276568ee7aa243db147723bc3
junior-delatorre <jrdelatorre7890@gmail.com>
2019-02-04 10:05:38 -0800

    hello.c

---
+++ c/hello.c
@@ -0,0 +1,17 @@
+// hello.c
+// complie gcc hello.c -o hello.o
+// run ./hello.o
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello,world!");
+	printf("****************\n");
+	printf("jr        jr   \n");
+	printf("  jr jr jr     \n");
+	printf("    jr jr      \n");
+	printf("      jr       \n");
+	
+	return 0;
+
+}
